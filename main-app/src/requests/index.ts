257e660c340cb97510c709b130d711997bab707c
carlomagg <obijurumagnus@gmail.com>
2025-09-04 07:11:07 +0100

    Remove main-app folder

--- main-app/src/requests/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as AuthRequest } from "./auth.request";
-export { default as StudentRequest } from "./students.request";
-export { default as TutorRequest } from "./tutor.request";
-export { default as ClientRequest } from "./client.request";
-export { default as AdminRequest } from "./admin.request";
